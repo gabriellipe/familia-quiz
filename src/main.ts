53fff4ec33781752d1f6972ecf2234821f05c35c
Gabriel Del <gabrieldelc.barros@gmail.com>
2025-09-28 20:35:27 -0300

    fix: Tela em branco

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,15 @@
+import 'zone.js';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http';
+import { provideZoneChangeDetection } from '@angular/core';
 
 import { AppComponent } from './app/app.component';
 import { routes } from './app/app.routes';
 
 bootstrapApplication(AppComponent, {
   providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideHttpClient(),
   ]
