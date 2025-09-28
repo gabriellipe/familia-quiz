43b6060126d94d38b181e2961f7bf40172caab5a
Gabriel Del <gabrieldelc.barros@gmail.com>
2025-09-28 20:29:32 -0300

    fix: start

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http';
-import { importProvidersFrom } from '@angular/core';
 
 import { AppComponent } from './app/app.component';
 import { routes } from './app/app.routes';
